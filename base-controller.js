d2223f3b670d6e858b069019c3c5195b32e5513a
Sean <seanphelan97@gmail.com>
2018-10-14 19:24:35 -0600

    add BaseController class (does not support auth)

---
+++ base-controller.js
@@ -0,0 +1,36 @@
+
+class BaseController {
+
+  constructor(model, key) {
+    this.model = model;
+    this.modelName = model.modelName.toLowerCase();
+    this.key = key;
+  }
+
+  create(data) {
+
+  }
+
+  read(id) {
+
+  }
+
+  list() {
+
+  }
+
+  update(id, data) {
+
+  }
+
+  remove(id) {
+
+  }
+
+  route() {
+    const router = new Router();
+
+    router.get("
+
+    return router;
+}
