516eeeb97805febc774dbe6dc3e59598a207684f
Sean Phelan <seanphelan97@gmail.com>
2018-10-19 12:50:08 -0600

    modified GET for basic search

--- routes/entries.js
+++ routes/entries.js
@@ -12,21 +12,16 @@ const router = express.Router();
 router.use('/', passport.authenticate('jwt', { session: false, failWithError: true }));
 
 router.get('/', (req, res, next) => {
-  console.log('reached /api/entries');
-  if(!req.user) {
-    console.log('req.user is not set');
-    console.log('skipping route');
-    return next();
-  }
-  console.log('req.user is set');
-  console.log('req.user contents: ', req.user);
-
-
-  // TODO: add filtering
   const {id: userId} = req.user;
+  const {filter} = req.query;
+
+  const dbFilter = {userId};
+  if(filter) {
+    dbFilter.content = {$regex: filter, $options: 'i'};
+  }
 
   // FIXME: don't hardcode the limit
-  return Entry.find({userId}).limit(1000)
+  return Entry.find(dbFilter).limit(1000)
     .then(results => {
       return res.json(results.map(result => result.toObject()));
     })
@@ -59,7 +54,7 @@ router.post('/', requireFields(['content']), (req, res, next) => {
   };
   Entry.create(newEntry)
     .then(data => {
-      // FIXME: RETURNS THE WRONG DATA (before serialize/toObject, including the user's password)
+      // FIXME: toObject has to be called here
       res.location(`${req.originalUrl}/${data.id}`).status(201).json(data.toObject());
     }).catch(err => next(err));
 });
